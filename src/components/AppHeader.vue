8ce80dd69f4f2bede66d3e2b15a5e19fd45df35d
mattemutti <mattemutti@gmail.com>
2024-04-03 14:55:49 +0200

    update function in state.js

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,6 @@
 <script>
-import { toHandlers } from 'vue';
+
 import { state } from '../state';
-import axios from 'axios'
 
 export default {
 
@@ -11,29 +10,18 @@ export default {
 		return {
 			state,
 			movies: [],
-			searchText: '',
-
 
 		}
 	},
 	methods: {
 
-		searchTitle() {
-			axios
-				.get('https://api.themoviedb.org/3/search/movie?api_key=80957307a07de1c274de94c5be154b47&query=' + this.searchText)
-				.then(resp => {
-					console.log(resp);
-					this.movies = resp
-					console.log(this.searchText);
-				})
-		}
 	}
 }
 </script>
 <template>
 
-	<input type="text" placeholder="digit" v-model="searchText">
-	<button @click="searchTitle">{{ state.message }}</button>
+	<input type="text" placeholder="digit" v-model="state.searchText">
+	<button @click="state.searchTitle">{{ state.message }}</button>
 
 </template>
 <style></style>
--- src/state.js
+++ src/state.js
@@ -1,12 +1,22 @@
-import { reactive } from 'vue'
+import { reactive } from 'vue';
+import axios from 'axios';
 
 export const state = reactive({
 	message: 'Search',
+	searchText: '',
 
 
 	searchTitle() {
 		console.log('funzione richiamata');
 		// qui richiamo la funzione che ricerca il titolo inserito dall'utente nel input, nel database di db movie
+		axios
+			.get('https://api.themoviedb.org/3/search/movie?api_key=80957307a07de1c274de94c5be154b47&query=' + this.searchText)
+			.then(resp => {
+				console.log(resp);
+				this.movies = resp
+				console.log(this.searchText);
+			})
+
 	}
 
 
