109766d6ab6521270493ebf3b836b7fd89c234b9
mattemutti <mattemutti@gmail.com>
2024-04-03 14:36:36 +0200

    add call axios

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <script>
+import { toHandlers } from 'vue';
 import { state } from '../state';
+import axios from 'axios'
 
 export default {
 
@@ -7,8 +9,21 @@ export default {
 
 	data() {
 		return {
-			state
+			state,
+			movies: [],
 
+
+		}
+	},
+	methods: {
+
+		searchTitle() {
+			axios
+				.get('https://api.themoviedb.org/3/search/movie?api_key=80957307a07de1c274de94c5be154b47&query=matrix')
+				.then(resp => {
+					console.log(resp);
+					this.movies = resp
+				})
 		}
 	}
 }
@@ -16,7 +31,7 @@ export default {
 <template>
 
 	<input type="text" placeholder="digit">
-	<button>{{ state.message }}</button>
+	<button @click="searchTitle">{{ state.message }}</button>
 
 </template>
 <style></style>
