ff40d455dc436d146ae22261ea6459a8f50d9bed
mattemutti <mattemutti@gmail.com>
2024-04-03 11:27:47 +0200

    initial site

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+  name: 'App', 	// nome del componente
+
+  data() {
+    return {
+      message: 'By'
+    }
+  },
+  methods: {
+    // qui le funzioni
+  },
+  mounted() {
+    // qui i log nell hook mounted
+  }
+}
+</script>
+<template>
+
+  {{ message }}
+
+</template>
+<style></style>
