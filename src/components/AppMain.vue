e4a649e8c7d367d2bf9be54005299d86fa3e1559
mattemutti <mattemutti@gmail.com>
2024-04-04 16:35:17 +0200

    add initial info hover card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,11 +5,19 @@ export default {
 	data() {
 		return {
 			state,
+			showInfo: false,
 
 		}
 	},
 	methods: {
-
+		showOnInfoCard(index) {
+			this.showInfo = true;
+			console.log(this.showInfo, index);
+		},
+		showOff(index) {
+			this.showInfo = false;
+			console.log(this.showInfo, index);
+		}
 
 	}
 }
@@ -18,53 +26,56 @@ export default {
 
 	<div v-if="state.createListMovies === true">
 		<h2>Film</h2>
-		<ul v-for="movie in  state.movies ">
-			<img :src="state.url_Image + movie.poster_path" alt="">
+		<ul v-for="(movie, index) in  state.movies ">
+			<img :src="state.url_Image + movie.poster_path" alt="" @mouseover="showOnInfoCard(index)"
+				@mouseleave="showOff(index)">
 
-			<li>Titolo:{{ movie.title }}</li>
-			<li>Titolo Originale: {{ movie.original_title }}</li>
-			<li v-if="movie.original_language === 'en'">Lingua: <img class="w_flag" src="../assets/img/flag/gb.svg"
-					alt=""></li>
-			<li v-else-if="movie.original_language === 'it'">Lingua: <img class="w_flag" src="../assets/img/flag/it.svg"
-					alt=""></li>
-			<li v-else-if="movie.original_language === 'es'">Lingua: <img class="w_flag" src="../assets/img/flag/es.svg"
-					alt=""></li>
-			<li v-else-if="movie.original_language === 'fr'">Lingua: <img class="w_flag" src="../assets/img/flag/fr.svg"
-					alt=""></li>
-			<li v-else-if="movie.original_language === 'ja'">Lingua: <img class="w_flag" src="../assets/img/flag/jp.svg"
-					alt=""></li>
-			<li v-else>Lingua: {{ movie.original_language }}</li>
+			<div v-if="showInfo === true">
+				<li>Titolo:{{ movie.title }}</li>
+				<li>Titolo Originale: {{ movie.original_title }}</li>
+				<li v-if="movie.original_language === 'en'">Lingua: <img class="w_flag" src="../assets/img/flag/gb.svg"
+						alt=""></li>
+				<li v-else-if="movie.original_language === 'it'">Lingua: <img class="w_flag"
+						src="../assets/img/flag/it.svg" alt=""></li>
+				<li v-else-if="movie.original_language === 'es'">Lingua: <img class="w_flag"
+						src="../assets/img/flag/es.svg" alt=""></li>
+				<li v-else-if="movie.original_language === 'fr'">Lingua: <img class="w_flag"
+						src="../assets/img/flag/fr.svg" alt=""></li>
+				<li v-else-if="movie.original_language === 'ja'">Lingua: <img class="w_flag"
+						src="../assets/img/flag/jp.svg" alt=""></li>
+				<li v-else>Lingua: {{ movie.original_language }}</li>
 
-			<li v-if="((movie.vote_average / 2).toFixed(0)) == 0">Voto: <i class="fa-regular fa-star"></i><i
-					class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
-					class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i> {{ (movie.vote_average /
+				<li v-if="((movie.vote_average / 2).toFixed(0)) == 0">Voto: <i class="fa-regular fa-star"></i><i
+						class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
+						class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i> {{ (movie.vote_average /
 		2).toFixed(0) }}
-			</li>
-			<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 1">Voto: <i class="fa-solid fa-star"></i><i
-					class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
-					class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>{{ (movie.vote_average /
+				</li>
+				<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 1">Voto: <i class="fa-solid fa-star"></i><i
+						class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
+						class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>{{ (movie.vote_average /
 		2).toFixed(0) }}
-			</li>
-			<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 2">Voto: <i class="fa-solid fa-star"></i><i
-					class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
-					class="fa-regular fa-star"></i>{{ (movie.vote_average /
+				</li>
+				<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 2">Voto: <i class="fa-solid fa-star"></i><i
+						class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i
+						class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>{{ (movie.vote_average /
 		2).toFixed(0) }}
-			</li>
-			<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 3">Voto: <i class="fa-solid fa-star"></i><i
-					class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i
-					class="fa-regular fa-star"></i>{{ (movie.vote_average /
+				</li>
+				<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 3">Voto: <i class="fa-solid fa-star"></i><i
+						class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
+						class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>{{ (movie.vote_average /
 		2).toFixed(0) }}
-			</li>
-			<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 4">Voto: <i class="fa-solid fa-star"></i><i
-					class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
-					class="fa-regular fa-star"></i>{{ (movie.vote_average /
+				</li>
+				<li v-else-if="((movie.vote_average / 2).toFixed(0)) == 4">Voto: <i class="fa-solid fa-star"></i><i
+						class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
+						class="fa-regular fa-star"></i>{{ (movie.vote_average /
 		2).toFixed(0) }}
-			</li>
-			<li v-else="((movie.vote_average / 2 ).toFixed(0)) == 5">Voto: <i class="fa-solid fa-star"></i><i
-					class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
-					class="fa-solid fa-star"></i>{{ (movie.vote_average /
+				</li>
+				<li v-else="((movie.vote_average / 2 ).toFixed(0)) == 5">Voto: <i class="fa-solid fa-star"></i><i
+						class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
+						class="fa-solid fa-star"></i>{{ (movie.vote_average /
 		2).toFixed(0) }}
-			</li>
+				</li>
+			</div>
 		</ul>
 	</div>
 
