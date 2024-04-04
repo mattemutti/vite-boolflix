a0855af330c98da52453d8143bb8d70826dec74e
mattemutti <mattemutti@gmail.com>
2024-04-04 11:02:29 +0200

    add flag language

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,10 +16,24 @@ export default {
 		<ul v-for="movie in state.movies">
 			<li>Titolo:{{ movie.title }}</li>
 			<li>Titolo Originale: {{ movie.original_title }}</li>
-			<li>Lingua: {{ movie.original_language }}</li>
+			<li v-if="movie.original_language === 'en'">Lingua: <img class="w_flag" src="../assets/img/flag/gb.svg"
+					alt=""></li>
+			<li v-else-if="movie.original_language === 'it'">Lingua: <img class="w_flag" src="../assets/img/flag/it.svg"
+					alt=""></li>
+			<li v-else-if="movie.original_language === 'es'">Lingua: <img class="w_flag" src="../assets/img/flag/es.svg"
+					alt=""></li>
+			<li v-else-if="movie.original_language === 'fr'">Lingua: <img class="w_flag" src="../assets/img/flag/fr.svg"
+					alt=""></li>
+			<li v-else-if="movie.original_language === 'ja'">Lingua: <img class="w_flag" src="../assets/img/flag/jp.svg"
+					alt=""></li>
+			<li v-else>Lingua: {{ movie.original_language }}</li>
 			<li>Voto: {{ movie.vote_average }}</li>
 		</ul>
 	</div>
 
 </template>
-<style></style>
+<style>
+.w_flag {
+	width: 20px;
+}
+</style>
