fa98bcb92342dc32d68abc5944c008d15205513b
mattemutti <mattemutti@gmail.com>
2024-04-04 11:39:42 +0200

    add search serie tv

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,6 +13,7 @@ export default {
 <template>
 
 	<div v-if="state.createListMovies === true">
+		<h2>Film</h2>
 		<ul v-for="movie in state.movies">
 			<li>Titolo:{{ movie.title }}</li>
 			<li>Titolo Originale: {{ movie.original_title }}</li>
@@ -28,6 +29,26 @@ export default {
 					alt=""></li>
 			<li v-else>Lingua: {{ movie.original_language }}</li>
 			<li>Voto: {{ movie.vote_average }}</li>
+		</ul>
+	</div>
+
+	<div v-if="state.createListSerieTv === true">
+		<h2>Serie Tv</h2>
+		<ul v-for="serie in state.serieTv">
+			<li>Titolo:{{ serie.name }}</li>
+			<li>Titolo Originale: {{ serie.original_name }}</li>
+			<li v-if="serie.original_language === 'en'">Lingua: <img class="w_flag" src="../assets/img/flag/gb.svg"
+					alt=""></li>
+			<li v-else-if="serie.original_language === 'it'">Lingua: <img class="w_flag" src="../assets/img/flag/it.svg"
+					alt=""></li>
+			<li v-else-if="serie.original_language === 'es'">Lingua: <img class="w_flag" src="../assets/img/flag/es.svg"
+					alt=""></li>
+			<li v-else-if="serie.original_language === 'fr'">Lingua: <img class="w_flag" src="../assets/img/flag/fr.svg"
+					alt=""></li>
+			<li v-else-if="serie.original_language === 'ja'">Lingua: <img class="w_flag" src="../assets/img/flag/jp.svg"
+					alt=""></li>
+			<li v-else>Lingua: {{ serie.original_language }}</li>
+			<li>Voto: {{ serie.vote_average }}</li>
 		</ul>
 	</div>
 
--- src/state.js
+++ src/state.js
@@ -5,14 +5,18 @@ export const state = reactive({
 	nameButton: 'Search',
 	searchText: '',
 	createListMovies: false,
+	createListSerieTv: false,
 	movies: [],
+	serieTv: [],
+	api_url_movies: 'https://api.themoviedb.org/3/search/movie?api_key=80957307a07de1c274de94c5be154b47&query=',
+	api_url_tv: 'https://api.themoviedb.org/3/search/tv?api_key=80957307a07de1c274de94c5be154b47&query=',
 
 
 	searchTitle() {
 		//console.log('funzione richiamata');
 		// qui richiamo la funzione che ricerca il titolo inserito dall'utente nel input, nel database di db movie
 		axios
-			.get('https://api.themoviedb.org/3/search/movie?api_key=80957307a07de1c274de94c5be154b47&query=' + this.searchText)
+			.get(this.api_url_movies + this.searchText)
 			.then(resp => {
 				//console.log(resp);
 				this.movies = resp.data.results;
@@ -20,6 +24,15 @@ export const state = reactive({
 				this.createListMovies = true;
 				//console.log(this.createListMovies);
 				console.log(this.movies);
+			})
+		axios
+			.get(this.api_url_tv + this.searchText)
+			.then(resp => {
+				console.log('ricerca serie tv');
+				this.serieTv = resp.data.results;
+				console.log(this.serieTv);
+				this.createListSerieTv = true;
+
 			})
 
 	}
